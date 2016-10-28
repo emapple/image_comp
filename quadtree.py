617ae20c944e0e1a1e2bb82b40e228866f211bee
Elaad Applebaum <Elaad@applebaum.physics.rutgers.edu>
2016-10-28 19:25:27 -0400

    Updated, better-streamlined QuadTree class

--- quadtree.py
+++ quadtree.py
@@ -1,26 +1,5 @@
 import numpy as np
 
-#class Node:
-#    ROOT=0
-#    BRANCH=1
-#    LEAF=2
-#    def __init__(self,parent,rect):
-#        self.parent=parent
-#        self.children=[None,None,None,None]
-#        if self.parent==None:
-#            self.depth=0
-#        else:
-#            self.depth=parent.depth+1
-#        self.rect=rect
-#        i1,j1,i2,j2=rect
-#        if self.parent==None:
-#            self.type=Node.ROOT
-#        elif(i2-i1)<2:
-#            self.type=Node.LEAF
-#        else:
-#            self.type=Node.BRANCH
-#
-
 def maxdepth(data):
     """ Finds the tree depth of a square array """
 
@@ -34,19 +13,13 @@ def maxdepth(data):
 class QuadTree:
     leaves=[]
     allnodes=[]
-    def __init__(self,data,mini=None,minj=None,maxi=None,maxj=None,depth=None):
+    def __init__(self,data):
         self.data=np.array(data)
-        if depth is None:
-            depth=maxdepth(self.data)
-        if mini is None:
-            mini = 0
-        if minj is None:
-            minj=0
-        if maxi is None:
-            maxi=len(data)-1
-        if maxj is None:
-            maxj=len(data)-1
-        self.depth=depth
+        depth=maxdepth(self.data)
+        mini = 0
+        minj=0
+        maxi=len(data)-1
+        maxj=len(data)-1
         self.mini=mini
         self.minj=minj
         self.maxi=maxi
@@ -62,14 +35,29 @@ class QuadTree:
 
         if depth>0:
             data_nw=data[minj:int(np.ceil(midj)),mini:int(np.ceil(midi))]
-            data_ne=data[minj:int(np.ceil(midj)),int(np.floor(midi)):maxi+1]
-            data_sw=[int(np.floor(midj)):maxj+1,mini:int(np.ceil(midi))]
-            data_se=[int(np.floor(midj)):maxj+1,int(np.floor(midi)):maxi+1]
-            
-            #if data_nw.shape!=(1,1):
-                #self.children.append(QuadTree(data_nw,
+            data_ne=data[minj:int(np.ceil(midj)),int(np.ceil(midi)):maxi+1]
+            data_sw=data[int(np.ceil(midj)):maxj+1,mini:int(np.ceil(midi))]
+            data_se=data[int(np.ceil(midj)):maxj+1,int(np.ceil(midi)):maxi+1]
+            if data_nw.shape!=(0,0):
+                self.children.append(QuadTree(data_nw))
+            if data_ne.shape!=(0,0):
+                self.children.append(QuadTree(data_ne))
+            if data_sw.shape!=(0,0):
+                self.children.append(QuadTree(data_sw))
+            if data_se.shape!=(0,0):
+                self.children.append(QuadTree(data_se))
 
+    def displaytree(self,depth):
+        if depth==0:
+            print self.data
+        else:
+            for child in self.children:
+                child.displaytree(depth-1)
 
 if __name__=='__main__':
     table=np.array([[0,1,2,3],[5,3,6,4],[6,6,7,4],[6,4,7,5]])
     testtree=QuadTree(table)
+
+    testtree.displaytree(0)
+    testtree.displaytree(1)
+    testtree.displaytree(2)
