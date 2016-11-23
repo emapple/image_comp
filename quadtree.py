65a3cd67fb09eeaf093809ab2a0be28d09e09f48
Elaad Applebaum <Elaad@Elaad-Applebaum.local>
2016-11-23 16:25:12 -0600

    Added pruning function

--- quadtree.py
+++ quadtree.py
@@ -14,13 +14,13 @@ def maxdepth(data):
 class QuadTree:
     leaves=[]
     allnodes=[]
-    def __init__(self,data,parent_depth=None):
-        if parent_depth is None:
+    def __init__(self,data,parent=None):
+        self.parent=parent
+        if self.parent is None:
             self.depth=0
-            self.tot_depth=maxdepth(self.data) # total depth of quadtree, root to leaves
+            #self.tot_depth=maxdepth(self.data) # total depth of quadtree, root to leaves
         else:
-            self.depth=parent_depth+1
-            self.tot_depth=
+            self.depth=parent.depth+1
         self.data=np.array(data)
         self.value=np.mean(self.data)
         mdepth=maxdepth(self.data) # remaining levels of recursion
@@ -39,31 +39,34 @@ class QuadTree:
         self.sizei=self.maxi-self.mini
         self.sizej=self.maxj-self.minj
 
-        self.children=[]
-
         if mdepth>0:
+            self.children=[]
             data_nw=data[minj:int(np.ceil(midj)),mini:int(np.ceil(midi))]
             data_ne=data[minj:int(np.ceil(midj)),int(np.ceil(midi)):maxi+1]
             data_sw=data[int(np.ceil(midj)):maxj+1,mini:int(np.ceil(midi))]
             data_se=data[int(np.ceil(midj)):maxj+1,int(np.ceil(midi)):maxi+1]
             if data_nw.shape!=(0,0):
-                self.children.append(QuadTree(data_nw,parent_depth=self.depth))
+                self.children.append(QuadTree(data_nw,parent=self))
             if data_ne.shape!=(0,0):
-                self.children.append(QuadTree(data_ne,parent_depth=self.depth))
+                self.children.append(QuadTree(data_ne,parent=self))
             if data_sw.shape!=(0,0):
-                self.children.append(QuadTree(data_sw,parent_depth=self.depth))
+                self.children.append(QuadTree(data_sw,parent=self))
             if data_se.shape!=(0,0):
-                self.children.append(QuadTree(data_se,parent_depth=self.depth))
-            else:
-                self.children=None
+                self.children.append(QuadTree(data_se,parent=self))
+        else:
+            self.children=None
 
     def prune(self,tolerance):
-        if self.depth<max:
+        if self.children is not None:
             for child in self.children:
                 child.prune(tolerance)
-        else:
-            a
-                
+        else: # does this if no children
+            if self.parent.children is not None: # asserting siblings still exist; may have been removed in previous iteration
+                lnorm=np.sqrt(sum([child.value**2 for child in self.parent.children])) # calculates lnorm from siblings
+                diffs=[abs(lnorm-child.value)/lnorm for child in self.parent.children]
+                if max(diffs)<tolerance:
+                    self.parent.children=None
+        
 
     def displaytree(self,depth):
         if depth==0:
@@ -71,14 +74,7 @@ class QuadTree:
         else:
             for child in self.children:
                 child.displaytree(depth-1)
-
-#class TreeNodes:
-#    leaves=[]
-#    allnodes=[]
-#    def __init__(self,tree):
-#        if tree.children is None:
-#            leaves.append(tree)
-#        else:
+        
             
 
 
