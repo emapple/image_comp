1de0c3e04cb6f9ddf83f9c2c391d6a855ea64a86
Elaad Applebaum <Elaad@Elaad-Applebaum.local>
2016-11-23 15:28:11 -0600

    Added readfits in makeimage.py; temporary changes in quadtree.py

--- makeimage.py
+++ makeimage.py
@@ -1,4 +1,5 @@
 import numpy as np
+from astropy.io import fits
 
 def make2n(table,default=0):
     """ Takes a square table and makes it divisble for a quadtree"""
@@ -23,4 +24,8 @@ def make2n(table,default=0):
     table=np.array(table)
     return table
     
-
+def read_fits(filename):
+    """ Returns image data for all images in a fits file"""
+    hdulist=fits.open(filename)
+    data=np.array([hdulist[i].data for i in range(1,len(hdulist))])
+    return data
--- quadtree.py
+++ quadtree.py
@@ -3,19 +3,27 @@ import numpy as np
 def maxdepth(data):
     """ Finds the tree depth of a square array """
 
-    lside=len(data)
+    lside=float(len(data))
     n=0
     while lside>1:
         lside=lside/2
         n+=1
     return n
 
+
 class QuadTree:
     leaves=[]
     allnodes=[]
-    def __init__(self,data):
+    def __init__(self,data,parent_depth=None):
+        if parent_depth is None:
+            self.depth=0
+            self.tot_depth=maxdepth(self.data) # total depth of quadtree, root to leaves
+        else:
+            self.depth=parent_depth+1
+            self.tot_depth=
         self.data=np.array(data)
-        depth=maxdepth(self.data)
+        self.value=np.mean(self.data)
+        mdepth=maxdepth(self.data) # remaining levels of recursion
         mini = 0
         minj=0
         maxi=len(data)-1
@@ -33,19 +41,29 @@ class QuadTree:
 
         self.children=[]
 
-        if depth>0:
+        if mdepth>0:
             data_nw=data[minj:int(np.ceil(midj)),mini:int(np.ceil(midi))]
             data_ne=data[minj:int(np.ceil(midj)),int(np.ceil(midi)):maxi+1]
             data_sw=data[int(np.ceil(midj)):maxj+1,mini:int(np.ceil(midi))]
             data_se=data[int(np.ceil(midj)):maxj+1,int(np.ceil(midi)):maxi+1]
             if data_nw.shape!=(0,0):
-                self.children.append(QuadTree(data_nw))
+                self.children.append(QuadTree(data_nw,parent_depth=self.depth))
             if data_ne.shape!=(0,0):
-                self.children.append(QuadTree(data_ne))
+                self.children.append(QuadTree(data_ne,parent_depth=self.depth))
             if data_sw.shape!=(0,0):
-                self.children.append(QuadTree(data_sw))
+                self.children.append(QuadTree(data_sw,parent_depth=self.depth))
             if data_se.shape!=(0,0):
-                self.children.append(QuadTree(data_se))
+                self.children.append(QuadTree(data_se,parent_depth=self.depth))
+            else:
+                self.children=None
+
+    def prune(self,tolerance):
+        if self.depth<max:
+            for child in self.children:
+                child.prune(tolerance)
+        else:
+            a
+                
 
     def displaytree(self,depth):
         if depth==0:
@@ -53,6 +71,16 @@ class QuadTree:
         else:
             for child in self.children:
                 child.displaytree(depth-1)
+
+#class TreeNodes:
+#    leaves=[]
+#    allnodes=[]
+#    def __init__(self,tree):
+#        if tree.children is None:
+#            leaves.append(tree)
+#        else:
+            
+
 
 if __name__=='__main__':
     table=np.array([[0,1,2,3],[5,3,6,4],[6,6,7,4],[6,4,7,5]])
